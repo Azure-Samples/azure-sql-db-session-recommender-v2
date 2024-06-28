d4dad3f0c01b633d994208fcb95b7828a5e72e77
Davide Mauri <damauri@microsoft.com>
2024-06-28 09:55:42 +0200

    updated url to point to classic repo

--- database/setup-database.ps1
+++ database/setup-database.ps1
@@ -8,8 +8,8 @@ chmod a+x ./sqlpackage/sqlpackage
 
 Write-Host "Downloading dacpacs"
 
-Invoke-WebRequest -Uri https://github.com/azure-samples/azure-sql-db-session-recommender-v2/raw/main/database/session_recommender_v2.dacpac -OutFile session_recommender_v2.dacpac
-Invoke-WebRequest -Uri https://github.com/azure-samples/azure-sql-db-session-recommender-v2/raw/main/database/master.dacpac -OutFile master.dacpac
+Invoke-WebRequest -Uri https://github.com/azure-samples/azure-sql-db-session-recommender-v2/raw/classic/database/session_recommender_v2.dacpac -OutFile session_recommender_v2.dacpac
+Invoke-WebRequest -Uri https://github.com/azure-samples/azure-sql-db-session-recommender-v2/raw/classic/database/master.dacpac -OutFile master.dacpac
 
 Write-Host "Deploying database to $Env:DBSERVER with name $Env:DBNAME"
 
