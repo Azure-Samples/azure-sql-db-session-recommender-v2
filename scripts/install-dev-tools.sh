eb9de1c098e04b13cb328a737a1d0efe51412a7a
Davide Mauri <damauri@microsoft.com>
2024-07-17 09:59:38 +0200

    updated to latest versions

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -1,10 +1,10 @@
 sudo cp ./scripts/ms-repo.pref /etc/apt/preferences.d/
 
-export dotnet_version="6.0"
-export dab_version="0.9.7"
+export dotnet_version="8.0"
+export dab_version="1.1.7"
 export sqlcmd_version="1.6.0"
 export func_version="4"
-export sqlprj_version="0.1.15-preview"
+export sqlprj_version="0.1.19-preview"
 
 export debian_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 
@@ -15,13 +15,12 @@ sudo apt update
 
 sudo apt install dotnet-sdk-$dotnet_version -y
 
-npm i -g azure-functions-core-tools@$func_version --unsafe-perm true
+npm install -g azure-functions-core-tools@$func_version --unsafe-perm true
 
 npm install -g @azure/static-web-apps-cli
 
 dotnet tool install -g microsoft.sqlpackage
 dotnet new install Microsoft.Build.Sql.Templates::$sqlprj_version
-
 
 dotnet tool install -g Microsoft.DataApiBuilder --version $dab_version
 
